4678e652edf3e27ac7a1bee619e3b71686c27d85
tungdeptrai <nguyentungdev99@gmail.com>
2022-07-02 15:52:23 +0700

    added addToCart feature

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,18 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 import './CartIcon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleIsCartOpen = () => { setIsCartOpen(!isCartOpen); };
-  // const close = () => {
-  //   setIsCartOpen(false);
-  // };
+  // close dropdown when click outside
+  //My solution for close cart-dropdown when click outside (cart-icon.component.jsx)
+  let dropdownRef = useRef();
+  useEffect(() => {
+    document.addEventListener('mousedown', (e) => {
+      if (!dropdownRef.current.contains(e.target)) {
+        setIsCartOpen(false);
+      }
+    });
+  });
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+    <div className='cart-icon-container' onClick={toggleIsCartOpen} ref={dropdownRef}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
